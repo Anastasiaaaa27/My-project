6af8cc5822bb808c63d5cb032ca1d14c70edddaf
Anastasiaaaa27 <118101365+Anastasiaaaa27@users.noreply.github.com>
2024-08-26 21:13:55 +0500

    Add files via upload
    В консоли каждый кадр выводится слово «crickets».

---
+++ Crickets.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    private void Update()
+    {
+        // Выводим слово "crickets" в консоль каждый кадр
+        Debug.Log("crickets");
+    }
+}
